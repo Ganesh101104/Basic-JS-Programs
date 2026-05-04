e5ea5c06a835ff9ecff759eaec62de77a3ab3ab9
Maltumkar Ganesh <ganesh101104@gmail.com>
2026-05-04 14:50:38 +0530

    Ganesh

---
+++ switch.js
@@ -0,0 +1,19 @@
+let n1=45;
+let n2=69;
+let operator="+";
+switch(operator){
+    case "+":
+        console.log(n1+n2);
+        break;
+    case "-":
+        console.log(n1-n2);
+        break;
+    case "*":
+        console.log(n1*n2);
+        break;
+    case "/":
+        console.log(n1/n2);
+        break;
+    default:
+        console.log("Invalid operator");
+}
